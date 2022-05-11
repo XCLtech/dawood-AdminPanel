bd8df2318f5d539b8e6396a79b0bbc1fae199e1a
wabbi-khan <kwahab789@gmail.com>
2022-05-12 00:19:56 +0500

    testing

--- components/shared/tables/TableOrdersItems.jsx
+++ components/shared/tables/TableOrdersItems.jsx
@@ -6,8 +6,14 @@ import Axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import Router from "next/router";
+import Spinner from "~/components/spinner/Spinner";
+import style from "./style.module.css";
 
 const TableOrdersItems = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchBags = async () => {
@@ -19,6 +25,7 @@ const TableOrdersItems = () => {
           `https://dawoodddocker.herokuapp.com/api/v1/order`
         );
         setData(data.data.data);
+        setLoading(true);
       } catch (error) {
         console.log(error);
       }
@@ -46,84 +53,120 @@ const TableOrdersItems = () => {
     });
   };
 
-  const tableItemsView = data.map((item) => {
-    let badgeView, fullfillmentView;
-    const menuView = (
-      <Menu>
-        <Menu.Item key={0}>
-          <a className='dropdown-item' href='#'>
-            Edit
-          </a>
-        </Menu.Item>
-        <Menu.Item key={0}>
-          <a className='dropdown-item' href='#'>
-            <i className='icon-t'></i>
-            Delete
-          </a>
-        </Menu.Item>
-      </Menu>
-    );
-    if (item.payment) {
-      badgeView = <span className='ps-badge success'>Paid</span>;
-    } else {
-      badgeView = <span className='ps-badge gray'>Unpaid</span>;
-    }
-    switch (item.fullfillment) {
-      case "In Progress":
-        fullfillmentView = (
-          <span className='ps-fullfillment warning'>In Progress</span>
+  const tableItemsView = loading ? (
+    data
+      .filter((item) => {
+        if (searchTerm == "") {
+          return item;
+        } else if (
+          item.id
+            .toString()
+            .toLowerCase()
+            .includes(searchTerm.toString().toLowerCase())
+          // item.CategoryId.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return item;
+        }
+      })
+      .map((item) => {
+        let badgeView, fullfillmentView;
+        const menuView = (
+          <Menu>
+            <Menu.Item key={0}>
+              <a className='dropdown-item' href='#'>
+                Edit
+              </a>
+            </Menu.Item>
+            <Menu.Item key={0}>
+              <a className='dropdown-item' href='#'>
+                <i className='icon-t'></i>
+                Delete
+              </a>
+            </Menu.Item>
+          </Menu>
         );
-        break;
-      case "Cancel":
-        fullfillmentView = (
-          <span className='ps-fullfillment danger'>Cancel</span>
-        );
-        break;
-      default:
-        fullfillmentView = (
-          <span className='ps-fullfillment success'>delivered</span>
-        );
-        break;
-    }
-    return (
-      <tr key={item.id}>
-        {/* <td>{item.id}</td> */}
-        <td>
-          {/* <button className="ps-dropdown__toggle" onClick={()=>postDelete(item.id)}> */}
-          {/* <Link href="/orders/order-detail/"> */}
+        if (item.payment) {
+          badgeView = <span className='ps-badge success'>Paid</span>;
+        } else {
+          badgeView = <span className='ps-badge gray'>Unpaid</span>;
+        }
+        switch (item.fullfillment) {
+          case "In Progress":
+            fullfillmentView = (
+              <span className='ps-fullfillment warning'>In Progress</span>
+            );
+            break;
+          case "Cancel":
+            fullfillmentView = (
+              <span className='ps-fullfillment danger'>Cancel</span>
+            );
+            break;
+          default:
+            fullfillmentView = (
+              <span className='ps-fullfillment success'>delivered</span>
+            );
+            break;
+        }
+        return (
+          <tr key={item.id}>
+            {/* <td>{item.id}</td> */}
+            <td>
+              {/* <button className="ps-dropdown__toggle" onClick={()=>postDelete(item.id)}> */}
+              {/* <Link href="/orders/order-detail/"> */}
 
-          <h4 style={{ display: "inline" }}>DRWE{item.id}</h4>
+              <h4 style={{ display: "inline" }}>DRWE{item.id}</h4>
 
-          {/* </Link> */}
-          {/* </button> */}
-        </td>
+              {/* </Link> */}
+              {/* </button> */}
+            </td>
 
-        <td>
-          <strong> {item.createdAt}</strong>
-        </td>
-        {/* <td>{item.updatedAt}</td> */}
-        {/* <td>{fullfillmentView}</td> */}
-        <td>
-          <strong>{item.total}</strong>
-        </td>
-        <td>
-          <strong>{item.subTotal}</strong>
-        </td>
-        <td>
-          {" "}
-          <button className='ps-dropdown__toggle' onClick={() => send(item.id)}>
-            View Detail
-          </button>
-        </td>
-        <td></td>
-        {/* <td>
+            <td>
+              <strong> {item.createdAt}</strong>
+            </td>
+            {/* <td>{item.updatedAt}</td> */}
+            {/* <td>{fullfillmentView}</td> */}
+            <td>
+              <strong>{item.total}</strong>
+            </td>
+            <td>
+              <strong>{item.subTotal}</strong>
+            </td>
+            <td>
+              {" "}
+              <button
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                  border: "none",
+                  padding: "5px 10px",
+                  borderRadius: "5px",
+                }}
+                className='ps-dropdown__toggle'
+                onClick={() => send(item.id)}
+              >
+                View Detail
+              </button>
+            </td>
+            <td></td>
+            {/* <td>
                     <DropdownAction />
                 </td> */}
-      </tr>
-    );
-  });
+          </tr>
+        );
+      })
+  ) : (
+    <Spinner />
+  );
   return (
     <div className='table-responsive'>
+      <input
+        className={style.input}
+        type='text'
+        placeholder='Search Order by ID...'
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
       <table className='table ps-table'>
         <thead>
           <tr>
--- components/shared/tables/TableProjectItems.jsx
+++ components/shared/tables/TableProjectItems.jsx
@@ -1,182 +1,130 @@
-// import React from 'react';
-// import DropdownAction from '~/components/elements/basic/DropdownAction';
-import React from 'react';
-import Link from 'next/link';
-import { Menu } from 'antd';
-import DropdownAction from '~/components/elements/basic/DropdownAction';
-import Axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import Axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import style from "./style.module.css";
+import Spinner from "~/components/spinner/Spinner";
 
 const TableProjectItems = () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const fetchBags = async () => {
-            try {
-                const data = await Axios.get(
-                    // `http://localhost:8082/api/v1/product`
-                    // `https://dawoodbackend.herokuapp.com/api/v1/product/id/5`
-                    `http://dawoodddocker.herokuapp.com/api/v1/product`
-                );
-                setData(data.data.data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchBags();
-    }, []);
-    // const productItems = [
-    //     {
-    //         name: 'Herschel Leather Duffle Bag In Brown Color',
-    //         sku: 'AB123456789-1',
-    //         stock: 'true',
-    //         price: '£125.30',
-    //         date: '2019/11/06',
-    //         categories: [
-    //             {
-    //                 name: 'Bags',
-    //             },
-    //             {
-    //                 name: 'Clothing & Apparel',
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         name: 'Apple iPhone Retina 6s Plus 64GB',
-    //         sku: 'CD987654316-1',
-    //         stock: 'true',
-    //         price: '£1,249.99',
-    //         date: '2018/12/11',
-    //         categories: [
-    //             {
-    //                 name: 'Computers & Technologies',
-    //             },
-    //             {
-    //                 name: 'Technologies',
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         name: 'Marshall Kilburn Portable Wireless Speaker',
-    //         sku: 'SF1133569600-1',
-    //         stock: 'true',
-    //         price: '£36.78',
-    //         date: '2018/12/11',
-    //         categories: [
-    //             {
-    //                 name: 'Babies & Moms',
-    //             },
-    //             {
-    //                 name: 'Refrigerators',
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         name: 'Xbox One Wireless Controller Black Color',
-    //         sku: 'AB123456788',
-    //         stock: 'false',
-    //         price: '£55.30',
-    //         date: '2018/12/11',
-    //         categories: [
-    //             {
-    //                 name: 'Accessories',
-    //             },
-    //             {
-    //                 name: 'Air Conditioners',
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         name: 'Grand Slam Indoor Of Show Jumping Novel',
-    //         sku: 'AB1234567899',
-    //         stock: 'false',
-    //         price: '£32.39',
-    //         date: '2018/12/11',
-    //         categories: [
-    //             {
-    //                 name: 'Books & Office',
-    //             },
-    //             {
-    //                 name: 'Cars & Motocycles',
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         name: 'Rayban Rounded Sunglass Brown Color',
-    //         sku: 'AB123456783',
-    //         stock: 'true',
-    //         price: '£321.39',
-    //         date: '2018/12/11',
-    //         categories: [
-    //             {
-    //                 name: 'Clothing & Apparel',
-    //             },
-    //             {
-    //                 name: 'Cars & Motocycles',
-    //             },
-    //         ],
-    //     },
-    // ];
-    
-    const postDelete = async (id)  => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
 
-     let data=await  Axios.delete(
-            // `http://localhost:8082/api/v1/product`
-            // `https://dawoodbackend.herokuapp.com/api/v1/product/id/5`
-            `http://localhost:8080/api/v1/product/delete/${id}`
-            )
-            // .then(res=>console.log("deleted",res)).catch(err=>console.log("error",err))
-        console.log(id)
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchBags = async () => {
+      try {
+        const data = await Axios.get(
+          // `http://localhost:8082/api/v1/product`
+          // `https://dawoodbackend.herokuapp.com/api/v1/product/id/5`
+          `http://dawoodddocker.herokuapp.com/api/v1/product`
+        );
+        setData(data.data.data);
+        setLoading(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchBags();
+  }, []);
+
+  const postDelete = async (id) => {
+    let data = await Axios.delete(
+      // `http://localhost:8082/api/v1/product`
+      // `https://dawoodbackend.herokuapp.com/api/v1/product/id/5`
+      `http://localhost:8080/api/v1/product/delete/${id}`
+    );
+    // .then(res=>console.log("deleted",res)).catch(err=>console.log("error",err))
+    console.log(id);
+  };
+
+  const tableItems = loading ? (
+    data
+      .filter((item) => {
+        if (searchTerm == "") {
+          return item;
+        } else if (
+          item.title.toLowerCase().includes(searchTerm.toLowerCase())
+          // item.CategoryId.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return item;
         }
-    
-    const tableItems = data.map((item, index) => {
+      })
+      .map((item, index) => {
         // console.log(item.id)
         let badgeView;
         if (item.stock) {
-            badgeView = <span className="ps-badge success">Stock</span>;
+          badgeView = <span className='ps-badge success'>Stock</span>;
         } else {
-            badgeView = <span className="ps-badge gray">Out of stock</span>;
+          badgeView = <span className='ps-badge gray'>Out of stock</span>;
         }
         return (
-            <tr key={item.sku}>
-                {/* <td>{index + 1}</td> */}
-                <td>
-                    <a href="#">
-                        <strong>{item.id}</strong>
-                    </a>
-                </td>
-                <td>{item.title}</td>
-                <td>{item.price}</td>
-                <td>
-                    <strong>{item.CategoryId }</strong>
-                </td>
-               
-                {/* <td>{item.date}</td> */}
-                <td>
-                <button onClick={()=>postDelete(item.id)}>
+          <tr key={item.sku}>
+            {/* <td>{index + 1}</td> */}
+            <td>
+              <a href='#'>
+                <strong>{item.id}</strong>
+              </a>
+            </td>
+            <td>{item.title}</td>
+            <td>{item.Bar_code}</td>
+            <td>{item.price}</td>
+            <td>
+              <strong>{item.CategoryId}</strong>
+            </td>
 
-                    Delete</button>
-                    {/* <DropdownAction /> */}
-                </td>
-            </tr>
+            {/* <td>{item.date}</td> */}
+            <td>
+              <button
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                  border: "none",
+                  padding: "5px 10px",
+                  borderRadius: "5px",
+                }}
+                onClick={() => postDelete(item.id)}
+              >
+                Delete
+              </button>
+              {/* <DropdownAction /> */}
+            </td>
+          </tr>
         );
-    });
-    return (
-        <div className="table-responsive">
-            <table className="table ps-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>CategoryID</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>{tableItems}</tbody>
+      })
+  ) : (
+    <Spinner />
+  );
+  return (
+    <>
+      <input
+        className={style.input}
+        type='text'
+        placeholder='Search Product...'
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      <div className='conta'>
+        <div className='row'>
+          <div className='table-responsive'>
+            <table className='table ps-table'>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>BarCode</th>
+                  <th>Price</th>
+                  <th>CategoryID</th>
+                  <th>Delete product</th>
+                </tr>
+              </thead>
+              <tbody>{tableItems}</tbody>
             </table>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default TableProjectItems;
